7942b9121596ac0c676efe73c47519a9fe67c5e0
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:40:34 -0300

    add javaS

---
+++ aula.js
@@ -0,0 +1,16 @@
+//Aqui é um alert
+alert("Ola mundo");
+
+/*
+Eu Sabo
+*/
+
+// imprimindo mensagem na tela
+document.write("Seu nome é Pedro");
+
+// Usando consele
+console.log("Ola tudo bem");
+
+//criando a primeira variavel
+let mensagem = "Ola, Mundo"; 
+console.log(" Esse consele é da variavel",mensagem);
