1efb486858418fbb641e807ac93d5993f7fcc786
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 08:47:41 -0300

    add nova exemplo

---
+++ exemplo2.js
@@ -0,0 +1,19 @@
+let valor = 7;
+
+console.log(valor);
+
+valor--; 
+
+console.log(valor)
+
+valor -= 2; 
+
+console.log(valor);
+
+valor *= 2;
+
+console.log("multiplicação", valor);
+
+valor /= 2;
+
+console.log("/", valor)
