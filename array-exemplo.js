5fb3e19da178d3baffee0c14f8d2697ca3d87307
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 09:47:46 -0300

    add novo ex

---
+++ array-exemplo.js
@@ -0,0 +1,5 @@
+const time = ["Flamengo", "Paulista", "Botafogo"];
+
+time.push("Real Madrid"); 
+
+console.table(time)
---
+++ exercicio.js
@@ -0,0 +1,11 @@
+let i = 0;   
+let X = "Subistitua X pela informação correta"; 
+
+while (i <= 10) { 
+    console.log(X)
+    i++
+}
+
+for(let i =0; i <=10; i++){
+    console.log(`Esta é a ${i +1}ª vez que essa frase é exibida.`)
+}
